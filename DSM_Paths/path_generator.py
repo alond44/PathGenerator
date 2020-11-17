6d2a2dc551841c8faa7b09877b929a9c30fc60e0
alond44 <alondvir.14@gmail.com>
2020-11-17 20:10:14 +0200

    resize fix and main function skeleton.

--- DSM_Paths/path_generator.py
+++ DSM_Paths/path_generator.py
@@ -73,7 +73,7 @@ class PathGenerator:
             new_dsm = [[0 for j in range(self._map_side)] for i in range(self._map_side)]
             for x in range(0, len(new_dsm)):
                 for y in range(0, len(new_dsm)):
-                    maxi = -1
+                    maxi = -np.inf
                     for i in range(0, multi):
                         for j in range(0, multi):
                             x_idx = x*multi + i
@@ -96,6 +96,29 @@ class PathGenerator:
     def gen_path_dist(self):
         # TODO: implement
         pass
+
+    def gen_path_time(self):
+        # TODO: implement
+        pass
+
+    def gen_paths(self, flag, constrain, num=1, to_print=False):
+        """
+                Enhancing the dsm resolution by the given multiplier
+                 Parameters
+                ---------
+                flag : str
+                    Will hold either 'd' for distance constrain or 't' for time constrain.
+                constrain : float
+                    The paths' length in meters for distance constrained paths or the travel time for time
+                    in seconds constrained paths.
+                num : int
+                    The number of paths wanted.
+                to_print : bool
+                    Pass True to print the resulting paths over the dsm map.
+        """
+        if flag != 'd' and flag != 't':
+            print('Wrong flag option!')
+            return
 
     def print_path(self, points=None, path_color='r', path_style='--'):
         """
@@ -124,34 +147,64 @@ class PathGenerator:
         plt.show()
 
 
-if __name__ == "__main__":
-    """
-    Inputpath = 'C:/Users/alond/PycharmProjects/DSM_Paths'
-    FileName = 'dsm_binary'
-    dsm_ = create_map(Inputpath, FileName)
-    pg = PathGenerator(50, 100, dsm_)
-    print('Initial map: ')
-    pg.print_map_size()
-    pg.print_path([[], []])
-    pg.resize_dsm(2)
-    print('After expansion: ')
-    pg.print_map_size()
-    pg.print_path([[], []])
-    pg.resize_dsm(2, False)
-    print('After reverting expansion: ')
-    pg.print_map_size()
-    pg.print_path([[], []])
-    """
+def expansion_test():
     dsm_ = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
     for x in range(len(dsm_)):
         print(dsm_[x])
 
-    _map_side *= multi
-    new_dsm = [[0 for j in range(self._map_side)] for i in range(self._map_side)]
+    multi = 2
+    _map_side = multi * len(dsm_)
+    new_dsm = [[0 for j in range(_map_side)] for i in range(_map_side)]
     for x in range(0, len(new_dsm)):
         for y in range(0, len(new_dsm)):
-            new_dsm[x][y] = self._dsm[int(x / multi)][int(y / multi)]
-    self._pixel_dist = self._pixel_dist / multi
+            new_dsm[x][y] = dsm_[int(x / multi)][int(y / multi)]
+    # self._pixel_dist = self._pixel_dist / multi
+    dsm_ = new_dsm
+
+    print('After expansion: ')
+    for x in range(len(new_dsm)):
+        print(new_dsm[x])
+
+    prev_side = _map_side
+    _map_side = int(prev_side / multi) if prev_side % multi == 0 else 1 + int(prev_side / multi)
+    new_dsm = [[0 for j in range(_map_side)] for i in range(_map_side)]
+    for x in range(0, len(new_dsm)):
+        for y in range(0, len(new_dsm)):
+            maxi = -1
+            for i in range(0, multi):
+                for j in range(0, multi):
+                    x_idx = x * multi + i
+                    y_idx = y * multi + j
+                    val = 0 if x_idx >= prev_side or y_idx >= prev_side else dsm_[x_idx][y_idx]
+                    maxi = max(maxi, val)
+            new_dsm[x][y] = maxi
+    # self._pixel_dist = self._pixel_dist * multi
+
+    print('After reverting expansion: ')
+    for x in range(len(new_dsm)):
+        print(new_dsm[x])
+
+
+if __name__ == "__main__":
+    Inputpath = 'C:/Users/alond/PycharmProjects/DSM_Paths'
+    FileName = 'dsm_binary'
+    dsm_ = create_map(Inputpath, FileName)
+    # dsm_ = np.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
+    pg = PathGenerator(50, 100, dsm_)
+    print('Initial map: ')
+    # for x in range(len(pg._dsm)):
+    #    print(pg._dsm[x])
+    pg.print_path([[], []])
+    pg.resize_dsm(2)
+    print('After expansion: ')
+    # for x in range(len(pg._dsm)):
+    #    print(pg._dsm[x])
+    pg.print_path([[], []])
+    pg.resize_dsm(2, False)
+    print('After reverting expansion: ')
+    # for x in range(len(pg._dsm)):
+    #    print(pg._dsm[x])
+    pg.print_path([[], []])
 
     # pg2 = PathGenerator(50, 100, dsm_)
     # pg2.print_path()
